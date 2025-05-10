3f200fcf5e7ac77749c155b4d5f4967f85fca653
Trần Đức Vũ <93377252+Tranducvu1@users.noreply.github.com>
2025-05-10 17:14:04 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,18 @@
-import type { NextConfig } from "next"
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Sử dụng cấu hình 'export' thay vì next export
   experimental: {
+    // Nếu bạn muốn sử dụng các tính năng thử nghiệm, bạn có thể bật tại đây
     // reactCompiler: true,
   },
   images: {
     remotePatterns: [
-      { hostname: "pbs.twimg.com" },
-      { hostname: "yt3.googleusercontent.com" },
-      { hostname: "cdn.prod.website-files.com" },
-      { hostname: "assets.aceternity.com" },
-      { hostname: "images.unsplash.com" },
+      { hostname: 'pbs.twimg.com' },
+      { hostname: 'yt3.googleusercontent.com' },
+      { hostname: 'cdn.prod.website-files.com' },
+      { hostname: 'assets.aceternity.com' },
+      { hostname: 'images.unsplash.com' },
     ],
   },
 }
